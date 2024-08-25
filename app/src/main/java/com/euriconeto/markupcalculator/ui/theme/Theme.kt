c33a971b321c2859d76806a2291f149c93e143e9
euric <euriconascimentofn@gmail.com>
2024-08-24 21:20:50 -0300

    feat: modify theme

--- app/src/main/java/com/euriconeto/markupcalculator/ui/theme/Theme.kt
+++ app/src/main/java/com/euriconeto/markupcalculator/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.euriconeto.markupcalculator.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,43 +8,74 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = lightGreen,
-    onPrimary = Color.White,
-    primaryContainer = darkGreen,
-    onPrimaryContainer = Color.Black,
-    secondary = cleanGreen,
-    onSecondary = Color.White,
-    secondaryContainer = swampGreen,
-    onSecondaryContainer = Color.Black,
-    tertiary = babyBlue,
-    onTertiary = Color.White,
-    tertiaryContainer = oilBlue,
-    onTertiaryContainer = Color.Black,
-    error = red,
-    errorContainer = lightRed,
-    background = grey
+    primary = lightBlue,
+    onPrimary = closeBlue,
+    secondary = oilBlue,
+    onSecondary = darkOilBlue,
+    tertiary = lightPurple,
+    onTertiary = darkPurple,
+    primaryContainer = darkOilBlueInverse,
+    onPrimaryContainer = oilBlueInverse,
+    secondaryContainer = darkGreyBlue,
+    onSecondaryContainer = lightGreyBlue,
+    tertiaryContainer = purple,
+    onTertiaryContainer = whitePurple,
+    error = cleanRed,
+    onError = blackRed,
+    errorContainer = bloodRed,
+    onErrorContainer = lightRed,
+    surfaceDim = blackGray,
+    surface = blackGray,
+    surfaceBright = greyNormal,
+    surfaceContainerLowest = dark,
+    surfaceContainerLow = darkGrey90,
+    surfaceContainer = darkGrey70,
+    surfaceContainerHigh = darkGrey60,
+    surfaceContainerHighest = darkGrey50,
+    onSurface = grey40,
+    onSurfaceVariant = grey05,
+    outline = grey25,
+    outlineVariant = grey75,
+    inverseSurface = grey40,
+    inverseOnSurface = grey80,
+    inversePrimary = darkCyan
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = darkGreen,
-    onPrimary = Color.White,
-    primaryContainer = lightGreen,
-    onPrimaryContainer = Color.Black,
-    secondary = swampGreen,
-    onSecondary = Color.White,
-    secondaryContainer = cleanGreen,
-    onSecondaryContainer = Color.Black,
-    tertiary = oilBlue,
-    onTertiary = Color.White,
-    tertiaryContainer = babyBlue,
-    onTertiaryContainer = Color.Black,
+    primary = darkCyan,
+    onPrimary = white,
+    secondary = rustCopper,
+    onSecondary = white,
+    tertiary = purpleTertiary,
+    onTertiary = white,
+    primaryContainer = blueBaby,
+    onPrimaryContainer = deepBlue,
+    secondaryContainer = cleanRustBlue,
+    onSecondaryContainer = deepRust,
+    tertiaryContainer = lavender,
+    onTertiaryContainer = deepPurple,
     error = red,
+    onError = white,
     errorContainer = lightRed,
-    background = grey
+    onErrorContainer = darkRed,
+    surfaceDim = grey,
+    surface = lightGrey,
+    surfaceBright = lightGrey,
+    surfaceContainerLowest = white,
+    surfaceContainerLow = grey10,
+    surfaceContainer = grey20,
+    surfaceContainerHigh = grey30,
+    surfaceContainerHighest = grey40,
+    onSurface = darkGray,
+    onSurfaceVariant = grey70,
+    outline = grey60,
+    outlineVariant = grey50,
+    inverseSurface = grey80,
+    inverseOnSurface = grey55,
+    inversePrimary = lightBabyBlue
 )
 
 @Composable
